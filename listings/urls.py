33478654141b897f8eca75785c02c8b926169e87
Kingsley Williams <kingsley.c.williams@gmail.com>
2019-06-17 18:38:36 +0300

    added single listing

--- listings/urls.py
+++ listings/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='listings'), #will be /listings
+    path('', views.index, name='listings'), #will be /listings since it has been declared in the main url.py urlpatterns.
     path('<int:listing_id>', views.listing, name='listing'), #will be /listings/id
     path('search', views.search, name='search'),
 ]
